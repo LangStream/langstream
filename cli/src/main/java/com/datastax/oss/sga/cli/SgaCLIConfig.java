be238df1187e6901233ff2eb4755bf776bfcc84a
Nicolò Boschi <boschi1997@gmail.com>
2023-07-04 01:37:04 +0200

    Add CLI utility (#20)

--- cli/src/main/java/com/datastax/oss/sga/cli/SgaCLIConfig.java
+++ cli/src/main/java/com/datastax/oss/sga/cli/SgaCLIConfig.java
@@ -2,8 +2,10 @@ package com.datastax.oss.sga.cli;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class SgaCLIConfig {
 
     @JsonProperty(required = true)

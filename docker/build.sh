5d96c04e673d5e32af20e6fd9f79c9aaa396aa4f
Nicolò Boschi <boschi1997@gmail.com>
2023-07-12 00:32:17 +0200

    compile

--- docker/build.sh
+++ docker/build.sh
@@ -4,5 +4,5 @@ set -e
 # Control plane
 ./mvnw package jib:dockerBuild -pl webservice -am -DskipTests
 echo "Image built successfully: datastax/sga-control-plane:latest-dev"
-./mvnw package -pl :k8s-deployer-core -am -DskipTests
+./mvnw package -pl :k8s-deployer-operator -am -DskipTests
 echo "Image built successfully: datastax/sga-deployer:latest-dev"

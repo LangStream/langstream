98ba7c8e2fe3c531b55644300018f75c0ed957a5
Nicolò Boschi <boschi1997@gmail.com>
2023-07-12 15:32:27 +0200

    Create deployer runtime docker image (#33)

---
+++ runtime/src/main/java/com/datastax/oss/sga/runtime/deployer/RuntimeDeployer.java
@@ -0,0 +1,60 @@
+package com.datastax.oss.sga.runtime.deployer;
+
+import com.datastax.oss.sga.api.model.Application;
+import com.datastax.oss.sga.api.runtime.ClusterRuntimeRegistry;
+import com.datastax.oss.sga.api.runtime.ExecutionPlan;
+import com.datastax.oss.sga.api.runtime.PluginsRegistry;
+import com.datastax.oss.sga.impl.deploy.ApplicationDeployer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.nio.file.Path;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * This is the main entry point for the deployer runtime.
+ */
+@Slf4j
+public class RuntimeDeployer {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static ErrorHandler errorHandler = error -> {
+        log.error("Unexpected error", error);
+        System.exit(-1);
+    };
+
+    public interface ErrorHandler {
+        void handleError(Throwable error);
+    }
+
+    public static void main(String... args) {
+        try {
+            if (args.length < 1) {
+                throw new IllegalArgumentException("Missing runtime deployer configuration");
+            }
+            Path configPath = Path.of(args[0]);
+            log.info("Loading configuration from {}", configPath);
+            final RuntimeDeployerConfiguration configuration =
+                    MAPPER.readValue(configPath.toFile(), RuntimeDeployerConfiguration.class);
+
+            final String applicationName = configuration.getName();
+            final String applicationConfig = configuration.getApplication();
+
+            final Application appInstance =
+                    MAPPER.readValue(applicationConfig, Application.class);
+
+            ApplicationDeployer deployer = ApplicationDeployer
+                    .builder()
+                    .registry(new ClusterRuntimeRegistry())
+                    .pluginsRegistry(new PluginsRegistry())
+                    .build();
+
+            log.info("Deploying application {}", applicationName);
+            final ExecutionPlan implementation = deployer.createImplementation(appInstance);
+            deployer.deploy(implementation);
+            log.info("Application {} deployed", applicationName);
+        } catch (Throwable error) {
+            errorHandler.handleError(error);
+            return;
+        }
+    }
+}
+

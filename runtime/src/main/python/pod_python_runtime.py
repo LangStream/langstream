165a424b95f3037a3add4713734d2381057b67a1
Eric Hare <ericrhare@gmail.com>
2023-07-17 06:03:13 -0700

    Add latest changes, include execution

--- runtime/src/main/python/pod_python_runtime.py
+++ runtime/src/main/python/pod_python_runtime.py
@@ -1,4 +1,6 @@
 import yaml
+import subprocess
+import os
 
 class PodPythonRuntime:
     def __init__(self, config_file):
@@ -6,18 +8,39 @@ class PodPythonRuntime:
         self.config = None
 
     def load_config(self):
-        with open(self.config_file, 'r') as file:
-            self.config = yaml.safe_load(file)
+        if not os.path.exists(self.config_file):
+            print("No configuration file provided. Using default `add.py`")
+        else:
+            with open(self.config_file, 'r') as file:
+                self.config = yaml.safe_load(file)
 
-    def run(self):
+    def execute(self, file_path):
         if self.config is None:
-            print("Configuration not loaded. Please call 'load_config' before running.")
-            return
+            pass
+            # TODO: We probably want to disable execution if config isn't set. But for demo purposes, allowing.
+        try:
+            # Execute the Python file
+            process = subprocess.Popen(['python', file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output, error = process.communicate()
 
-        # Add your logic here to process the configuration and run the pod runtime.
+            # Check if there was any error during execution
+            if error:
+                return error.decode('utf-8')
+            
+            # Return the output
+            return output.decode('utf-8')
+        
+        except Exception as e:
+            return str(e)
+
 
 if __name__ == '__main__':
-    # Example usage
+    # Example configuration loading
     runtime = PodPythonRuntime('config.yaml')
     runtime.load_config()
-    runtime.run()
+
+    # Example execution
+    file_path = 'add.py'  # TODO: This comes from the configuration i assume
+    result = runtime.execute(file_path)
+
+    print(result)

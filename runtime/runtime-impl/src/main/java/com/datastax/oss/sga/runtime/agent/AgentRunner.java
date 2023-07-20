3bd6d059812ed5ac4351b959bcf59037e5519d4e
Nicolò Boschi <boschi1997@gmail.com>
2023-07-20 13:09:36 +0200

    change run cmd

--- runtime/runtime-impl/src/main/java/com/datastax/oss/sga/runtime/agent/AgentRunner.java
+++ runtime/runtime-impl/src/main/java/com/datastax/oss/sga/runtime/agent/AgentRunner.java
@@ -173,7 +173,7 @@ public class AgentRunner
         // copy input/output to standard input/output of the java process
         // this allows to use "kubectl logs" easily
         ProcessBuilder processBuilder = new ProcessBuilder(
-                "python", "-m", "sga_runtime", podRuntimeConfiguration.toAbsolutePath().toString())
+                "python", "-m", "python_runtime", podRuntimeConfiguration.toAbsolutePath().toString())
                 .inheritIO()
                 .redirectOutput(ProcessBuilder.Redirect.INHERIT)
                 .redirectError(ProcessBuilder.Redirect.INHERIT);

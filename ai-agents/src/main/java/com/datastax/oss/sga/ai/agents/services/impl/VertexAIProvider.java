422340ba1126975fa8da8330e9e1d11a9ecd26e7
Enrico Olivelli <enrico.olivelli@datastax.com>
2023-07-25 16:04:44 +0200

    Implement Vertex AI Chat Completions and add support for Google Service Account JSON files (#118)

--- ai-agents/src/main/java/com/datastax/oss/sga/ai/agents/services/impl/VertexAIProvider.java
+++ ai-agents/src/main/java/com/datastax/oss/sga/ai/agents/services/impl/VertexAIProvider.java
@@ -1,28 +1,44 @@
 package com.datastax.oss.sga.ai.agents.services.impl;
 
 import com.datastax.oss.sga.ai.agents.services.ServiceProviderProvider;
+import com.datastax.oss.streaming.ai.completions.ChatChoice;
+import com.datastax.oss.streaming.ai.completions.ChatCompletions;
+import com.datastax.oss.streaming.ai.completions.ChatMessage;
 import com.datastax.oss.streaming.ai.completions.CompletionsService;
 import com.datastax.oss.streaming.ai.embeddings.EmbeddingsService;
 import com.datastax.oss.streaming.ai.services.ServiceProvider;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.auth.oauth2.CredentialRefreshListener;
+import com.google.api.client.auth.oauth2.TokenErrorResponse;
+import com.google.api.client.auth.oauth2.TokenResponse;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.stream.Collectors;
 
 @Slf4j
 public class VertexAIProvider implements ServiceProviderProvider {
 
-    private static final String URL_TEMPLATE = "%s/v1/projects/%s/locations/%s/publishers/google/models/%s:predict";
+    private static final String VERTEX_URL_TEMPLATE = "%s/v1/projects/%s/locations/%s/publishers/google/models/%s:predict";
 
     private static final ObjectMapper MAPPER = new ObjectMapper()
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
@@ -36,12 +52,12 @@ public class VertexAIProvider implements ServiceProviderProvider {
 
         Map<String, Object> config = (Map<String, Object>) agentConfiguration.get("vertex");
         String token = (String) config.get("token");
+        String serviceAccountJson = (String) config.get("serviceAccountJson");
         String url = (String) config.get("url");
         String project = (String) config.get("project");
         String region = (String) config.get("region");
 
-
-        return new VertexAIServiceProvider(url, project, region, token);
+        return new VertexAIServiceProvider(url, project, region, token, serviceAccountJson);
     }
 
     private static class VertexAIServiceProvider implements ServiceProvider {
@@ -54,7 +70,11 @@ public class VertexAIProvider implements ServiceProviderProvider {
 
         private final String token;
 
-        public VertexAIServiceProvider(String url, String project, String region, String token) {
+        private final GoogleCredential googleCredential;
+        private final ScheduledExecutorService refreshTokenExecutor;
+
+        @SneakyThrows
+        public VertexAIServiceProvider(String url, String project, String region, String token, String serviceAccountJson) {
             if (url == null || url.isEmpty()) {
                 url = "https://" + region + "-aiplatform.googleapis.com";
             }
@@ -62,14 +82,77 @@ public class VertexAIProvider implements ServiceProviderProvider {
 
             this.project = project;
             this.region = region;
-            this.token = token;
+
+            if (token != null && !token.trim().isEmpty()) {
+                log.info("Using static Access Token to connect to Vertex AI");
+                this.token = token.trim();
+                this.googleCredential = null;
+                this.refreshTokenExecutor = null;
+            } else if (serviceAccountJson != null && !serviceAccountJson.trim().isEmpty()) {
+                log.info("Getting a token using OAuth2 from a Google Service Account");
+                this.token = null;
+                this.refreshTokenExecutor = Executors.newSingleThreadScheduledExecutor();
+                this.googleCredential =
+                        GoogleCredential.fromStream(new ByteArrayInputStream(serviceAccountJson.getBytes(StandardCharsets.UTF_8)))
+                                .createScoped(Set.of("https://www.googleapis.com/auth/cloud-platform"))
+                                .toBuilder()
+                                .addRefreshListener(new CredentialRefreshListener() {
+                                    @Override
+                                    public void onTokenResponse(Credential credential, TokenResponse tokenResponse) throws IOException {
+                                        log.error("Token refreshed {}", tokenResponse);
+                                        Long expire = tokenResponse.getExpiresInSeconds();
+                                        if (expire != null) {
+                                            long refresh = expire - 120;
+                                            log.info("Token will expire in {} seconds, scheduling refresh in {} seconds", expire, refresh);
+                                            scheduleRefreshToken(refresh);
+                                        }
+                                    }
+                                    @Override
+                                    public void onTokenErrorResponse(Credential credential, TokenErrorResponse tokenErrorResponse) throws IOException {
+                                            log.error("Error while refreshing token. {}", tokenErrorResponse);
+                                    }
+                                }).build();
+
+                // get the initial token
+                // an error here fails the pod
+                this.googleCredential.refreshToken();
+            } else {
+                throw new IllegalArgumentException("You have to pass the access token or the service account json file");
+            }
+
             this.httpClient = HttpClient.newHttpClient();
+        }
+
+        private void scheduleRefreshToken(long refresh) {
+            refreshTokenExecutor.schedule(() -> {
+                doRefreshToken();
+            }, refresh, java.util.concurrent.TimeUnit.SECONDS);
+        }
+
+        private void doRefreshToken()  {
+            try {
+                log.info("Refreshing token");
+                googleCredential.refreshToken();
+            } catch (Throwable error) {
+                log.error("Error while refreshing token", error);
+                // schedule again in 60 seconds
+                scheduleRefreshToken(60);
+            }
+        }
+
+
+        protected String getCurrentToken() {
+            if (token != null) {
+                return token;
+            }
+            return googleCredential.getAccessToken();
         }
 
 
         @Override
         public CompletionsService getCompletionsService(Map<String, Object> map) throws Exception {
-            throw new UnsupportedOperationException("CompletionsService not implemented for VertexAI");
+            String model = (String) map.getOrDefault("model", "chat-bison");
+            return new VertexAICompletionsService(model);
         }
 
         @Override
@@ -78,8 +161,135 @@ public class VertexAIProvider implements ServiceProviderProvider {
             return new VertexAIEmbeddingsService(model);
         }
 
+
+
+        private <R, T> T executeVertexCall(R requestEmbeddings, Class<T> responseType, String model) throws IOException, InterruptedException {
+            String finalUrl = VERTEX_URL_TEMPLATE
+                    .formatted(url, project, region, model);
+            String request = MAPPER.writeValueAsString(requestEmbeddings);
+            log.info("URL: {}", finalUrl);
+            log.info("Request: {}", request);
+
+            HttpResponse<String> response = httpClient.send(HttpRequest.newBuilder()
+                    .uri(URI.create(finalUrl))
+                    .header("Authorization", "Bearer " + getCurrentToken())
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(request))
+                    .build(), HttpResponse.BodyHandlers.ofString());
+
+            String body = response.body();
+            log.info("Response: {}", body);
+            return MAPPER.readValue(body, responseType);
+        }
+
         @Override
         public void close() {
+        }
+
+        private class VertexAICompletionsService implements CompletionsService {
+            private final String model;
+
+            public VertexAICompletionsService(String model) {
+                this.model = model;
+            }
+
+            @Override
+            @SneakyThrows
+            public ChatCompletions getChatCompletions(List<ChatMessage> list, Map<String, Object> additionalConfiguration) {
+                // https://cloud.google.com/vertex-ai/docs/generative-ai/chat/chat-prompts
+                CompletionRequest request = new CompletionRequest();
+                CompletionRequest.Instance instance = new CompletionRequest.Instance();
+                request.instances.add(instance);
+                request.parameters = new HashMap<>();
+
+                if (additionalConfiguration.containsKey("temperature")) {
+                    request.parameters.put("temperature", additionalConfiguration.get("temperature"));
+                }
+                if (additionalConfiguration.containsKey("max-tokens")) {
+                    request.parameters.put("maxOutputTokens", additionalConfiguration.get("max-tokens"));
+                }
+                if (additionalConfiguration.containsKey("topP")) {
+                    request.parameters.put("topP", additionalConfiguration.get("topP"));
+                }
+                if (additionalConfiguration.containsKey("topK")) {
+                    request.parameters.put("topK", additionalConfiguration.get("topK"));
+                }
+
+                instance.context = "";
+                instance.examples = new ArrayList<>();
+                instance.messages = list
+                        .stream()
+                        .map(m -> {
+                            CompletionRequest.Message message = new CompletionRequest.Message();
+                            message.content = m.getContent();
+                            message.author = m.getRole();
+                            return message;
+                        }).collect(Collectors.toList());
+
+                Predictions predictions = executeVertexCall(request, Predictions.class, model);
+                ChatCompletions completions = new ChatCompletions();
+                completions.setChoices(predictions.predictions.stream().map(p -> {
+                    if (!p.candidates.isEmpty()) {
+                        ChatChoice completion = new ChatChoice();
+                        completion.setMessage(new ChatMessage(p.candidates.get(0).author)
+                                .setContent(p.candidates.get(0).content));
+                        return completion;
+                    } else {
+                        ChatChoice completion = new ChatChoice();
+                        completion.setMessage(new ChatMessage("")
+                                .setContent(""));
+                        return completion;
+                    }
+                }).collect(Collectors.toList()));
+                return completions;
+            }
+
+
+            @Data
+            static class CompletionRequest {
+                Map<String, Object> parameters;
+
+                List<Instance> instances = new ArrayList<>();
+
+                @Data
+                static class Instance {
+                    String context;
+                    List<Example> examples = new ArrayList<>();
+                    List<Message> messages = new ArrayList<>();
+                }
+
+                @Data
+                static class Example {
+                    Map<String, Object> input;
+                    Map<String, Object> output;
+                }
+
+                @Data
+                static class Message {
+                    String author;
+                    String content;
+                }
+
+            }
+
+            @Data
+            static class Predictions {
+
+                List<Prediction> predictions;
+                @Data
+                static class Prediction {
+
+                    List<Candidate> candidates = new ArrayList<>();
+
+                    @Data
+                    static class Candidate {
+                        String author;
+                        String content;
+                    }
+
+                }
+            }
+
         }
 
         private class VertexAIEmbeddingsService implements EmbeddingsService {
@@ -93,59 +303,47 @@ public class VertexAIProvider implements ServiceProviderProvider {
             @SneakyThrows
             public List<List<Double>> computeEmbeddings(List<String> list) {
                 // https://cloud.google.com/vertex-ai/docs/generative-ai/embeddings/get-text-embeddings#generative-ai-get-text-embedding-drest
-
                 RequestEmbeddings requestEmbeddings = new RequestEmbeddings(list);
-
-                String finalUrl = URL_TEMPLATE
-                        .formatted(url, project, region, model);
-                String request = MAPPER.writeValueAsString(requestEmbeddings);
-                log.info("URL: {}", finalUrl);
-                log.info("Request: {}", request);
-                HttpResponse<String> response = httpClient.send(HttpRequest.newBuilder()
-                        .uri(URI.create(finalUrl))
-                        .header("Authorization", "Bearer " + token)
-                        .header("Content-Type", "application/json")
-                        .POST(HttpRequest.BodyPublishers.ofString(request))
-                        .build(), HttpResponse.BodyHandlers.ofString());
-
-                String body = response.body();
-                log.info("Response: {}", body);
-
-                Predictions predictions = MAPPER.readValue(body, Predictions.class);
+                Predictions predictions = executeVertexCall(requestEmbeddings, Predictions.class, model);
                 return predictions
                         .predictions.stream().map(p -> p.embeddings.values).collect(Collectors.toList());
             }
-        }
-    }
 
-    @Data
-    static class RequestEmbeddings {
-        public RequestEmbeddings(List<String> instances) {
-            this.instances = instances.stream().map(Instance::new).collect(Collectors.toList());
         }
+
+
 
         @Data
-        @AllArgsConstructor
-        static class Instance {
-            String content;
-        }
-        final List<Instance> instances;
-    }
-
-    @Data
-    static class Predictions {
-
-        List<Prediction> predictions;
-        @Data
-        static class Prediction {
-
-            Embeddings embeddings;
-
-            @Data
-            static class Embeddings {
-                List<Double> values;
+        static class RequestEmbeddings {
+            public RequestEmbeddings(List<String> instances) {
+                this.instances = instances.stream().map(Instance::new).collect(Collectors.toList());
             }
 
+            @Data
+            @AllArgsConstructor
+            static class Instance {
+                String content;
+            }
+            final List<Instance> instances;
         }
+
+        @Data
+        static class Predictions {
+
+            List<Prediction> predictions;
+            @Data
+            static class Prediction {
+
+                Embeddings embeddings;
+
+                @Data
+                static class Embeddings {
+                    List<Double> values;
+                }
+
+            }
+        }
+
     }
+
 }
---
+++ examples/applications/compute-vertexai/README.md
@@ -0,0 +1,68 @@
+# Using the Google Vertex AI API
+
+This sample application shows how to use the Vertex AI API to compute embeddings and perform chat completions.
+
+The Vertex AI Service Provider currently supports both the 'compute-ai-embeddings' and the 'ai-chat-completions' agents.
+
+This example shows how to use the 'compute-ai-embeddings' agent.
+
+## Prerequisites
+
+Get a Google Cloud account and create a project, enable the Vertex AI API and create a service account.
+
+Download the Service Account JSON file and put it into the `secrets.yaml` file.
+
+```
+secrets:  
+- name: vertex-ai
+  data:
+      url: https://us-central1-aiplatform.googleapis.com  
+      serviceAccountJson: xxx
+      region: us-central1
+      project: myproject
+```
+
+Alternatively, if you have already configured gcloud locally you can get a token from the gcloud CLI:
+
+```
+gcloud auth print-access-token
+```
+
+In this case you can put the token in the `secrets.yaml` file:
+
+```
+secrets:  
+- name: vertex-ai
+  data:
+      url: https://us-central1-aiplatform.googleapis.com  
+      token: xxx
+      region: us-central1
+      project: myproject
+```
+
+## Deploy the SGA application
+
+```
+./bin/sga-cli apps deploy test -app examples/applications/compute-vertex -i examples/instances/kafka-kubernetes.yaml -s examples/secrets/secrets.yaml
+```
+
+## Start a Producer
+```
+kubectl -n kafka run kafka-producer -ti --image=quay.io/strimzi/kafka:0.35.1-kafka-3.4.0 --rm=true --restart=Never -- bin/kafka-console-producer.sh --bootstrap-server my-cluster-kafka-bootstrap:9092 --topic input-topic
+```
+
+Insert a JSON with "id", "name" and "description":
+
+```
+{"id": 10, "name": "test", "description": "test"}
+```
+
+
+## Start a Consumer
+
+Start a Kafka Consumer on a terminal
+
+```
+kubectl -n kafka run kafka-consumer -ti --image=quay.io/strimzi/kafka:0.35.1-kafka-3.4.0 --rm=true --restart=Never -- bin/kafka-console-consumer.sh --bootstrap-server my-cluster-kafka-bootstrap:9092 --topic output-topic --from-beginning`
+```
+

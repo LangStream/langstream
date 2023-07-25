769e2d2d37bba108886a1bb253ba063b311b3b86
Christophe Bornet <cbornet@hotmail.com>
2023-07-25 12:02:53 +0200

    Rename to S3SourceAgentCodeProvider

--- agents/s3/src/main/java/com/dastastax/oss/sga/agents/s3/S3SourceAgentCodeProvider.java
+++ agents/s3/src/main/java/com/dastastax/oss/sga/agents/s3/S3SourceAgentCodeProvider.java
@@ -3,7 +3,7 @@ package com.dastastax.oss.sga.agents.s3;
 import com.datastax.oss.sga.api.runner.code.AgentCode;
 import com.datastax.oss.sga.api.runner.code.AgentCodeProvider;
 
-public class S3AgentCodeProvider implements AgentCodeProvider {
+public class S3SourceAgentCodeProvider implements AgentCodeProvider {
     @Override
     public boolean supports(String agentType) {
         return "s3-source".equals(agentType);

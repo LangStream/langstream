7df2c5f1b93bd063bda9935462e8fa2221ba5add
Christophe Bornet <cbornet@hotmail.com>
2023-07-25 11:58:29 +0200

    Add k8s S3 provider and example

---
+++ k8s-runtime/k8s-runtime-core/src/main/java/com/datastax/oss/sga/runtime/impl/k8s/agents/S3SourceAgentProvider.java
@@ -0,0 +1,25 @@
+package com.datastax.oss.sga.runtime.impl.k8s.agents;
+
+import static com.datastax.oss.sga.runtime.impl.k8s.KubernetesClusterRuntime.CLUSTER_TYPE;
+import com.datastax.oss.sga.api.model.AgentConfiguration;
+import com.datastax.oss.sga.api.runtime.ComponentType;
+import com.datastax.oss.sga.impl.common.AbstractAgentProvider;
+import java.util.List;
+import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Implements support for S3 Source Agents.
+ */
+@Slf4j
+public class S3SourceAgentProvider extends AbstractAgentProvider {
+
+    public S3SourceAgentProvider() {
+        super(Set.of("s3-source"), List.of(CLUSTER_TYPE, "none"));
+    }
+
+    @Override
+    protected final ComponentType getComponentType(AgentConfiguration agentConfiguration) {
+        return ComponentType.SOURCE;
+    }
+}

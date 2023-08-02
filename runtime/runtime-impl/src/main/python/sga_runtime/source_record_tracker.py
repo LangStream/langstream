88f7322869087edefb68252ed492de6f32be9a8f
Dave Fisher <dave.fisher@datastax.com>
2023-08-02 14:43:48 -0700

    Verify License Headers (#166)

--- runtime/runtime-impl/src/main/python/sga_runtime/source_record_tracker.py
+++ runtime/runtime-impl/src/main/python/sga_runtime/source_record_tracker.py
@@ -1,3 +1,5 @@
+#
+#
 # Copyright DataStax, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 from typing import List, Tuple
 
 from .api import CommitCallback, Record, Source

2f1857c4f9b1f6f3e4d3d9b74118e5e2df40c053
Nicolò Boschi <boschi1997@gmail.com>
2023-08-14 18:05:11 +0200

    try cli

--- cli/src/main/java/com/datastax/oss/sga/cli/commands/gateway/BaseGatewayCmd.java
+++ cli/src/main/java/com/datastax/oss/sga/cli/commands/gateway/BaseGatewayCmd.java
@@ -41,41 +41,39 @@ public abstract class BaseGatewayCmd extends BaseCmd {
 
 
     protected String computeQueryString(String credentials, Map<String, String> userParams, Map<String, String> options) {
-        String paramsPart = "";
-        String optionsPart = "";
+        Map<String, String> all = new HashMap<>();
         if (userParams != null) {
-            paramsPart = userParams.entrySet()
+            userParams.entrySet()
                     .stream()
-                    .map(e -> encodeParam(e, "param:"))
-                    .collect(Collectors.joining("&"));
+                    .forEach(e -> all.put("param:" + e.getKey(), e.getValue()));
 
         }
 
         if (options != null) {
-            optionsPart = options.entrySet()
+            options.entrySet()
                     .stream()
-                    .map(e -> encodeParam(e, "option:"))
-                    .collect(Collectors.joining("&"));
+                    .forEach(e -> all.put("option:" + e.getKey(), e.getValue()));
         }
 
-        String credentialsPart = "";
         if (credentials != null) {
-            credentialsPart = encodeParam("credentials", credentials, "");
+            all.put("credentials", credentials);
         }
 
-        return List.of(credentialsPart, paramsPart, optionsPart)
-                .stream().collect(Collectors.joining("&"));
+        return all.entrySet()
+                .stream()
+                .map(e -> encodeParam(e))
+                .collect(Collectors.joining("&"));
     }
 
 
 
-    private String encodeParam(Map.Entry<String, String> e, String prefix) {
-        return encodeParam(e.getKey(), e.getValue(), prefix);
+    private String encodeParam(Map.Entry<String, String> e) {
+        return encodeParam(e.getKey(), e.getValue());
     }
 
     @SneakyThrows
-    private String encodeParam(String key, String value, String prefix) {
-        return "%s=%s".formatted(prefix + key, URLEncoder.encode(value, "UTF-8"));
+    private String encodeParam(String key, String value) {
+        return "%s=%s".formatted(key, URLEncoder.encode(value, "UTF-8"));
     }
 
 

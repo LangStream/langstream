79f5525a39383257b860f0370c532d925eb6316e
Nicolò Boschi <boschi1997@gmail.com>
2023-08-16 17:07:28 +0200

    fix test

--- core/src/test/java/com/datastax/oss/sga/model/parser/ModelBuilderTest.java
+++ core/src/test/java/com/datastax/oss/sga/model/parser/ModelBuilderTest.java
@@ -32,7 +32,37 @@ class ModelBuilderTest {
     void testParseApplication1() throws Exception {
         Path path = Paths.get("src/test/resources/application1");
         final ModelBuilder.ApplicationWithPackageInfo applicationWithPackageInfo =
-                ModelBuilder.buildApplicationInstance(Arrays.asList(path), null, null);
+                ModelBuilder.buildApplicationInstance(Arrays.asList(path), """
+                                        instance:
+                                          globals:
+                                            tableName: "vsearch.products"
+                                          streamingCluster:
+                                            type: "pulsar"
+                                            configuration:
+                                              admin:
+                                                serviceUrl: "locahost:9999"
+                                              defaultTenant: "public"
+                                              defaultNamespace: "default"
+                                          computeCluster:
+                                            type: "pulsar"
+                        """, """
+                                                
+                        secrets:
+                          - name: "Database credentials"
+                            id: "astra-credentials"
+                            data:
+                              username: "my-db-username"
+                              password: "my-db-password"
+                              secureBundle: "base64:AAAAAAAAAAAAAAA"
+                          - name: "OpenAI Azure credentials"
+                            id: "openai-credentials"
+                            data:
+                               accessKey: "xxxxxxxxx"
+                          - name: "Hugging Face credentials"
+                            id: "hf-credentials"
+                            data:
+                              accessKey: "xxxxxxxxx"
+                        """);
         log.info("Parsed {}", applicationWithPackageInfo.getApplication());
         Application resolved = ApplicationPlaceholderResolver.resolvePlaceholders(applicationWithPackageInfo.getApplication());
         log.info("Resolved {}", resolved);

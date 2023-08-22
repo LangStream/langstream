34f685e6ef9e8ca6c1261a28ab7f8355369c5d05
Christophe Bornet <cbornet@hotmail.com>
2023-08-22 15:39:48 +0200

    Parse JAAS config in Python runtime to configure SASL

--- langstream-runtime/langstream-runtime-impl/src/main/python/langstream_runtime/tests/test_kafka_connection.py
+++ langstream-runtime/langstream-runtime-impl/src/main/python/langstream_runtime/tests/test_kafka_connection.py
@@ -21,6 +21,9 @@ import waiting
 import yaml
 from confluent_kafka import Consumer, Producer, TopicPartition
 from confluent_kafka.serialization import StringDeserializer, StringSerializer
+from kafka import KafkaConsumer
+from kafka.errors import KafkaError, UnrecognizedBrokerVersion, NoBrokersAvailable
+from testcontainers.core.waiting_utils import wait_container_is_ready
 from testcontainers.kafka import KafkaContainer
 
 from langstream.api import Record
@@ -28,41 +31,72 @@ from langstream.util import SimpleRecord, SingleRecordProcessor
 from langstream_runtime import kafka_connection
 from langstream_runtime import runtime
 
+SECURITY_PROTOCOL = 'SASL_PLAINTEXT'
+SASL_MECHANISM = 'PLAIN'
+USERNAME = 'admin'
+PASSWORD = 'admin-secret'
+
 
 def test_kafka_topic_connection():
-    with KafkaContainer(image='confluentinc/cp-kafka:7.4.0') as container:
+    with SaslKafkaContainer() \
+            .with_env("KAFKA_LISTENER_SECURITY_PROTOCOL_MAP", "BROKER:PLAINTEXT,PLAINTEXT:SASL_PLAINTEXT") \
+            .with_env("KAFKA_LISTENER_NAME_PLAINTEXT_SASL_ENABLED_MECHANISMS", SASL_MECHANISM) \
+            .with_env("KAFKA_LISTENER_NAME_PLAINTEXT_PLAIN_SASL_JAAS_CONFIG",
+                      "org.apache.kafka.common.security.plain.PlainLoginModule required " +
+                      f"username=\"{USERNAME}\" " +
+                      f"password=\"{PASSWORD}\" " +
+                      "user_admin=\"admin-secret\" " +
+                      "user_producer=\"producer-secret\" " +
+                      "user_consumer=\"consumer-secret\";") \
+            .with_env("KAFKA_SASL_JAAS_CONFIG", "org.apache.kafka.common.security.plain.PlainLoginModule required " +
+                                                f"username=\"{USERNAME}\" " +
+                                                f"password=\"{PASSWORD}\";") \
+            as container:
+
         input_topic = 'input-topic'
         output_topic = 'output-topic'
         bootstrap_server = container.get_bootstrap_server()
 
         config_yaml = f"""
-        streamingCluster:
-            type: kafka
-            configuration:
-                admin:
-                    bootstrap.servers: {bootstrap_server}
-        
-        input:
-            topic: {input_topic}
-        
-        output:
-            topic: {output_topic}
-              
-        agent:
-            applicationId: testApplicationId
-            agentId: testAgentId
-            configuration:
-                className: langstream_runtime.tests.test_kafka_connection.TestSuccessProcessor
-        """
+            streamingCluster:
+                type: kafka
+                configuration:
+                    admin:
+                        bootstrap.servers: {bootstrap_server}
+                        security.protocol: {SECURITY_PROTOCOL}
+                        sasl.jaas.config: "org.apache.kafka.common.security.plain.PlainLoginModule required username='{USERNAME}' password='{PASSWORD}';"
+                        sasl.mechanism: {SASL_MECHANISM}
+            input:
+                topic: {input_topic}
+
+            output:
+                topic: {output_topic}
+
+            agent:
+                applicationId: testApplicationId
+                agentId: testAgentId
+                configuration:
+                    className: langstream_runtime.tests.test_kafka_connection.TestSuccessProcessor
+            """
 
         config = yaml.safe_load(config_yaml)
 
-        producer = Producer({'bootstrap.servers': bootstrap_server})
+        producer = Producer({
+            'bootstrap.servers': bootstrap_server,
+            'sasl.mechanism': SASL_MECHANISM,
+            'security.protocol': SECURITY_PROTOCOL,
+            'sasl.username': USERNAME,
+            'sasl.password': PASSWORD
+        })
         producer.produce(input_topic, StringSerializer()('verification message'), headers=[('prop-key', b'prop-value')])
         producer.flush()
 
         consumer = Consumer({
             'bootstrap.servers': bootstrap_server,
+            'sasl.mechanism': SASL_MECHANISM,
+            'security.protocol': SECURITY_PROTOCOL,
+            'sasl.username': USERNAME,
+            'sasl.password': PASSWORD,
             'group.id': 'foo',
             'auto.offset.reset': 'earliest'
         })
@@ -198,3 +232,23 @@ class TestSuccessProcessor(SingleRecordProcessor):
 class TestFailingProcessor(SingleRecordProcessor):
     def process_record(self, record: Record) -> List[Record]:
         raise Exception('failed to process')
+
+
+class SaslKafkaContainer(KafkaContainer):
+    """"KafkaContainer with support for SASL in the waiting probe"""
+
+    def __init__(self, **kwargs):
+        super().__init__("confluentinc/cp-kafka:7.4.0", **kwargs)
+
+    @wait_container_is_ready(UnrecognizedBrokerVersion, NoBrokersAvailable, KafkaError, ValueError)
+    def _connect(self):
+        bootstrap_server = self.get_bootstrap_server()
+        consumer = KafkaConsumer(
+            group_id='test',
+            bootstrap_servers=[bootstrap_server],
+            security_protocol=SECURITY_PROTOCOL,
+            sasl_mechanism=SASL_MECHANISM,
+            sasl_plain_username=USERNAME,
+            sasl_plain_password=PASSWORD)
+        if not consumer.bootstrap_connected():
+            raise KafkaError("Unable to connect with kafka container!")

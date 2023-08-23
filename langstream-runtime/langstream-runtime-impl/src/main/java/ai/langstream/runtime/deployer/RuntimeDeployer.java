987b20673c5e17ab632e14397342ec5bc7bc3179
Nicolò Boschi <boschi1997@gmail.com>
2023-08-23 14:05:20 +0200

    Update RuntimeDeployer.java

--- langstream-runtime/langstream-runtime-impl/src/main/java/ai/langstream/runtime/deployer/RuntimeDeployer.java
+++ langstream-runtime/langstream-runtime-impl/src/main/java/ai/langstream/runtime/deployer/RuntimeDeployer.java
@@ -55,7 +55,7 @@ public class RuntimeDeployer {
 
             final String arg0 = args[0];
 
-            if (args.length < 4) {
+            if (args.length < 3) {
                 throw new IllegalArgumentException("Missing runtime deployer configuration");
             }
 

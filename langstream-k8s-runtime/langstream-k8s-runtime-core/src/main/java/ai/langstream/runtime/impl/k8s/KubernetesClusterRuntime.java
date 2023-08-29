57f8884a79fb31d7efeb1c960f7a3218a0e79650
Nicolò Boschi <boschi1997@gmail.com>
2023-08-29 12:12:38 +0200

    more

--- langstream-k8s-runtime/langstream-k8s-runtime-core/src/main/java/ai/langstream/runtime/impl/k8s/KubernetesClusterRuntime.java
+++ langstream-k8s-runtime/langstream-k8s-runtime-core/src/main/java/ai/langstream/runtime/impl/k8s/KubernetesClusterRuntime.java
@@ -31,6 +31,8 @@ import ai.langstream.impl.common.BasicClusterRuntime;
 import ai.langstream.impl.common.DefaultAgentNode;
 import ai.langstream.impl.k8s.KubernetesClientFactory;
 import ai.langstream.runtime.api.agent.RuntimePodConfiguration;
+import ai.langstream.impl.agents.ComposableAgentExecutionPlanOptimiser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import io.fabric8.kubernetes.api.model.Secret;
@@ -45,8 +47,9 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class KubernetesClusterRuntime extends BasicClusterRuntime {
-    static final ObjectMapper mapper =
-            new ObjectMapper().enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS);
+    static final ObjectMapper mapper = new ObjectMapper()
+            .enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     public static final String CLUSTER_TYPE = "kubernetes";
 
     static final List<ExecutionPlanOptimiser> OPTIMISERS =
@@ -194,12 +197,10 @@ public class KubernetesClusterRuntime extends BasicClusterRuntime {
         final AgentSpec agentSpec = new AgentSpec();
         agentSpec.setTenant(tenant);
         agentSpec.setApplicationId(applicationInstance.getApplicationId());
-        agentSpec.setImage(configuration.getImage());
-        agentSpec.setImagePullPolicy(configuration.getImagePullPolicy());
-        agentSpec.setResources(
-                new AgentSpec.Resources(
-                        ((DefaultAgentNode) agent).getResourcesSpec().parallelism(),
-                        ((DefaultAgentNode) agent).getResourcesSpec().size()));
+        agentSpec.setResources(new AgentSpec.Resources(
+                ((DefaultAgentNode) agent).getResourcesSpec().parallelism(),
+                ((DefaultAgentNode) agent).getResourcesSpec().size()
+        ));
         agentSpec.setAgentConfigSecretRef(secretName);
         agentSpec.setCodeArchiveId(codeStorageArchiveId);
         MessageDigest digest = MessageDigest.getInstance("SHA-256");

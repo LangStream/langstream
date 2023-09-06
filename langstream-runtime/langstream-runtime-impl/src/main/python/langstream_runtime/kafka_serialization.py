f80f39a3f048997e8fcf992513f0791f5ed0d102
Christophe Bornet <cbornet@hotmail.com>
2023-09-06 15:35:41 +0200

    Support consumer deserializer config in Python kafka connector

--- langstream-runtime/langstream-runtime-impl/src/main/python/langstream_runtime/kafka_serialization.py
+++ langstream-runtime/langstream-runtime-impl/src/main/python/langstream_runtime/kafka_serialization.py
@@ -19,16 +19,20 @@ import struct as _struct
 
 from confluent_kafka.serialization import (
     Serializer,
+    Deserializer,
     SerializationError,
     StringSerializer,
     DoubleSerializer,
     IntegerSerializer,
+    StringDeserializer,
+    DoubleDeserializer,
+    IntegerDeserializer,
 )
 
 
 class BooleanSerializer(Serializer):
     """
-    Serializes bool to boolean bytes.
+    Serializes bool to bytes.
 
     See Also:
         `BooleanSerializer Javadoc <https://github.com/apache/kafka/blob/trunk/clients/src/main/java/org/apache/kafka/common/serialization/BooleanSerializer.java>`_
@@ -36,7 +40,7 @@ class BooleanSerializer(Serializer):
 
     def __call__(self, obj, ctx=None):
         """
-        Serializes bool as boolean bytes.
+        Serializes bool as bytes.
 
         Args:
             obj (object): object to be serialized
@@ -57,7 +61,44 @@ class BooleanSerializer(Serializer):
         if obj is None:
             return None
 
-        return b"\x01" if obj else b"\x00"
+        try:
+            return _struct.pack(">?", obj)
+        except _struct.error as e:
+            raise SerializationError(str(e))
+
+
+class BooleanDeserializer(Deserializer):
+    """
+    Deserializes bool from bytes.
+
+    See Also:
+        `BooleanDeserializer Javadoc <https://github.com/apache/kafka/blob/trunk/clients/src/main/java/org/apache/kafka/common/serialization/BooleanDeserializer.java>`_
+    """  # noqa: E501
+
+    def __call__(self, value, ctx=None):
+        """
+        Deserializes bool from bytes.
+
+        Args:
+            value (bytes): bytes to be deserialized
+
+            ctx (SerializationContext): Metadata pertaining to the serialization
+                operation
+
+        Raises:
+            SerializerError if an error occurs during deserialization.
+
+        Returns:
+            bool if data is not None, otherwise None
+        """
+
+        if value is None:
+            return None
+
+        try:
+            return _struct.unpack(">?", value)[0]
+        except _struct.error as e:
+            raise SerializationError(str(e))
 
 
 class ShortSerializer(Serializer):
@@ -93,6 +134,40 @@ class ShortSerializer(Serializer):
 
         try:
             return _struct.pack(">h", obj)
+        except _struct.error as e:
+            raise SerializationError(str(e))
+
+
+class ShortDeserializer(Deserializer):
+    """
+    Deserializes int from int16 bytes.
+
+    See Also:
+        `ShortDeserializer Javadoc <https://github.com/apache/kafka/blob/trunk/clients/src/main/java/org/apache/kafka/common/serialization/ShortDeserializer.java>`_
+    """  # noqa: E501
+
+    def __call__(self, value, ctx=None):
+        """
+        Deserializes int from int16 bytes.
+
+        Args:
+            value (bytes): bytes to be deserialized
+
+            ctx (SerializationContext): Metadata pertaining to the serialization
+                operation
+
+        Raises:
+            SerializerError if an error occurs during deserialization.
+
+        Returns:
+            int if data is not None, otherwise None
+        """
+
+        if value is None:
+            return None
+
+        try:
+            return _struct.unpack(">h", value)[0]
         except _struct.error as e:
             raise SerializationError(str(e))
 
@@ -134,12 +209,46 @@ class LongSerializer(Serializer):
             raise SerializationError(str(e))
 
 
-class FloatSerializer(Serializer):
+class LongDeserializer(Deserializer):
     """
-    Serializes float to IEEE 754 binary32.
+    Deserializes int from int64 bytes.
 
     See Also:
-        `FloatSerializer Javadoc <https://docs.confluent.io/current/clients/javadocs/org/apache/kafka/common/serialization/FloatSerializer.html>`_
+        `LongDeserializer Javadoc <https://github.com/apache/kafka/blob/trunk/clients/src/main/java/org/apache/kafka/common/serialization/LongDeserializer.java>`_
+    """  # noqa: E501
+
+    def __call__(self, value, ctx=None):
+        """
+        Deserializes int from int32 bytes.
+
+        Args:
+            value (bytes): bytes to be deserialized
+
+            ctx (SerializationContext): Metadata pertaining to the serialization
+                operation
+
+        Raises:
+            SerializerError if an error occurs during deserialization.
+
+        Returns:
+            int if data is not None, otherwise None
+        """
+
+        if value is None:
+            return None
+
+        try:
+            return _struct.unpack(">q", value)[0]
+        except _struct.error as e:
+            raise SerializationError(str(e))
+
+
+class FloatSerializer(Serializer):
+    """
+    Serializes float to IEEE 754 binary32 bytes.
+
+    See Also:
+        `FloatSerializer Javadoc <https://github.com/apache/kafka/blob/trunk/clients/src/main/java/org/apache/kafka/common/serialization/FloatSerializer.java>`_
 
     """  # noqa: E501
 
@@ -170,12 +279,46 @@ class FloatSerializer(Serializer):
             raise SerializationError(str(e))
 
 
+class FloatDeserializer(Deserializer):
+    """
+    Deserializes float from IEEE 754 binary32 bytes.
+
+    See Also:
+        `FloatDeserializer Javadoc <https://github.com/apache/kafka/blob/trunk/clients/src/main/java/org/apache/kafka/common/serialization/FloatDeserializer.java>`_
+    """  # noqa: E501
+
+    def __call__(self, value, ctx=None):
+        """
+        Deserializes float from IEEE 754 binary32 bytes.
+
+        Args:
+            value (bytes): bytes to be deserialized
+
+            ctx (SerializationContext): Metadata pertaining to the serialization
+                operation
+
+        Raises:
+            SerializerError if an error occurs during deserialization.
+
+        Returns:
+            float if data is not None, otherwise None
+        """
+
+        if value is None:
+            return None
+
+        try:
+            return _struct.unpack(">f", value)[0]
+        except _struct.error as e:
+            raise SerializationError(str(e))
+
+
 class ByteArraySerializer(Serializer):
     """
     Serializes bytes.
 
     See Also:
-        `ByteArraySerializer Javadoc <https://docs.confluent.io/current/clients/javadocs/org/apache/kafka/common/serialization/ByteArraySerializer.html>`_
+        `ByteArraySerializer Javadoc <https://github.com/apache/kafka/blob/trunk/clients/src/main/java/org/apache/kafka/common/serialization/ByteArraySerializer.java>`_
 
     """  # noqa: E501
 
@@ -201,9 +344,48 @@ class ByteArraySerializer(Serializer):
             return None
 
         if not isinstance(obj, bytes):
-            raise SerializationError(f"ByteArraySerializer cannot serialize {obj}")
+            raise SerializationError(
+                f"ByteArraySerializer: a bytes-like object is required, not {type(obj)}"
+            )
 
         return obj
+
+
+class ByteArrayDeserializer(Deserializer):
+    """
+    Deserializes bytes.
+
+    See Also:
+        `ByteArrayDeserializer Javadoc <https://github.com/apache/kafka/blob/trunk/clients/src/main/java/org/apache/kafka/common/serialization/ByteArrayDeserializer.java>`_
+    """  # noqa: E501
+
+    def __call__(self, value, ctx=None):
+        """
+        Deserializes bytes.
+
+        Args:
+            value (bytes): bytes to be deserialized
+
+            ctx (SerializationContext): Metadata pertaining to the serialization
+                operation
+
+        Raises:
+            SerializerError if an error occurs during deserialization.
+
+        Returns:
+            float if data is not None, otherwise None
+        """
+
+        if value is None:
+            return None
+
+        if not isinstance(value, bytes):
+            raise SerializationError(
+                f"ByteArrayDeserializer: a bytes-like object is required, "
+                f"not {type(value)}"
+            )
+
+        return value
 
 
 STRING_SERIALIZER = StringSerializer()
@@ -214,3 +396,12 @@ LONG_SERIALIZER = LongSerializer()
 FLOAT_SERIALIZER = FloatSerializer()
 DOUBLE_SERIALIZER = DoubleSerializer()
 BYTEARRAY_SERIALIZER = ByteArraySerializer()
+
+STRING_DESERIALIZER = StringDeserializer()
+BOOLEAN_DESERIALIZER = BooleanDeserializer()
+SHORT_DESERIALIZER = ShortDeserializer()
+INTEGER_DESERIALIZER = IntegerDeserializer()
+LONG_DESERIALIZER = LongDeserializer()
+FLOAT_DESERIALIZER = FloatDeserializer()
+DOUBLE_DESERIALIZER = DoubleDeserializer()
+BYTEARRAY_DESERIALIZER = ByteArrayDeserializer()

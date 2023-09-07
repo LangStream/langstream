35b9376a5baecd2c80aae4c804222c2821757e20
Christophe Bornet <cbornet@hotmail.com>
2023-09-07 10:35:15 +0200

    Reformat Python examples (#370)

--- examples/applications/python-processor-embeddings/python/embeddings.py
+++ examples/applications/python-processor-embeddings/python/embeddings.py
@@ -20,17 +20,17 @@ import openai
 import json
 from openai.embeddings_utils import get_embedding
 
+
 class Embedding(object):
+    def init(self, config):
+        print("init", config)
+        openai.api_key = config["openaiKey"]
 
-  def init(self, config):
-    print('init', config)
-    openai.api_key = config["openaiKey"]
-
-  def process(self, records):
-    processed_records = []
-    for record in records:
-      embedding = get_embedding(record.value(), engine='text-embedding-ada-002')
-      result = {"input": str(record.value()), "embedding": embedding}
-      new_value = json.dumps(result)
-      processed_records.append((record, [SimpleRecord(value=new_value)]))
-    return processed_records
+    def process(self, records):
+        processed_records = []
+        for record in records:
+            embedding = get_embedding(record.value(), engine="text-embedding-ada-002")
+            result = {"input": str(record.value()), "embedding": embedding}
+            new_value = json.dumps(result)
+            processed_records.append((record, [SimpleRecord(value=new_value)]))
+        return processed_records

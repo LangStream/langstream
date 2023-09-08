37e44b780b8cb3c096057bfff81b0f01674a942c
Enrico Olivelli <enrico.olivelli@datastax.com>
2023-09-08 16:57:16 +0200

    Remove unrelated change

--- langstream-api/src/main/java/ai/langstream/api/model/Application.java
+++ langstream-api/src/main/java/ai/langstream/api/model/Application.java
@@ -33,10 +33,6 @@ public class Application {
     private Instance instance;
     private Secrets secrets;
 
-    public Map<String, Resource> getResources() {
-        return resources;
-    }
-
     @JsonIgnore
     public Module getModule(String module) {
         return modules.computeIfAbsent(module, Module::new);

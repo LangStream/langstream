321ca8f2b43f6e49d4d13ada9e39e0c7a4c357f4
Enrico Olivelli <enrico.olivelli@datastax.com>
2023-09-14 16:21:33 +0200

    yet another fix

--- langstream-api-gateway/src/main/java/ai/langstream/apigateway/runner/TopicConnectionsRuntimeProviderBean.java
+++ langstream-api-gateway/src/main/java/ai/langstream/apigateway/runner/TopicConnectionsRuntimeProviderBean.java
@@ -22,7 +22,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 

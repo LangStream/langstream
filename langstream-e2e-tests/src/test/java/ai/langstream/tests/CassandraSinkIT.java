a5bbdf87b95d216eb662447d72344e231acd5563
Nicolò Boschi <boschi1997@gmail.com>
2023-09-14 07:33:21 +0200

    fix

--- langstream-e2e-tests/src/test/java/ai/langstream/tests/CassandraSinkIT.java
+++ langstream-e2e-tests/src/test/java/ai/langstream/tests/CassandraSinkIT.java
@@ -18,17 +18,13 @@ package ai.langstream.tests;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.awaitility.Awaitility;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;

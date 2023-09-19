bbd3fcf996b5d36f9d38549f9d6b382ee1bc0a22
Enrico Olivelli <enrico.olivelli@datastax.com>
2023-09-19 14:28:57 +0200

    [logging] Do not print secrets while opening a connection to the Vector database

--- langstream-agents/langstream-ai-agents/src/main/java/com/datastax/oss/streaming/ai/datasource/CassandraDataSource.java
+++ langstream-agents/langstream-ai-agents/src/main/java/com/datastax/oss/streaming/ai/datasource/CassandraDataSource.java
@@ -74,7 +74,7 @@ public class CassandraDataSource implements QueryStepDataSource {
 
     @Override
     public void initialize(Map<String, Object> dataSourceConfig) {
-        log.info("Initializing AstraDBDataSource with config {}", dataSourceConfig);
+        log.info("Initializing CassandraDataSource with config {}", ConfigurationUtils.redactSecrets(dataSourceConfig));
         this.astraToken = ConfigurationUtils.getString("token", "", dataSourceConfig);
         this.astraEnvironment =
                 ConfigurationUtils.getString("environment", "PROD", dataSourceConfig);

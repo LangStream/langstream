aaffc2753976b1899d1001abe6c8d3be96acdb70
Nicolò Boschi <boschi1997@gmail.com>
2023-09-21 16:05:03 +0200

    create package

---
+++ bin/mini-langstream/package.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+version=$1
+if [ -z "$version" ]; then
+    echo "version is not set"
+    exit 1
+fi
+temp_dir=$(mktemp -d)
+mkdir -p $temp_dir/bin
+cp bin/mini-langstream/mini-langstream $temp_dir/bin/mini-langstream
+
+mkdir -p target
+target_filename=$(realpath target/mini-langstream-$version.zip)
+rm -rf $target_filename
+cd $temp_dir
+zip -r $target_filename .
+echo "Created $target_filename"

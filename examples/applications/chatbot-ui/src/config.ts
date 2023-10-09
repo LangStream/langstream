d1202c2eff1450a7e59a8788e821311d04ad5158
Mike Fortman <michael.fortman@datastax.com>
2023-10-09 15:23:36 -0500

    add chatbot-ui

---
+++ examples/applications/chatbot-ui/src/config.ts
@@ -0,0 +1,6 @@
+export const WEBSOCKET_URL = process.env.LANGSTREAM_WEBSOCKET_URL || '';
+export const APP_NAME = process.env.LANGSTREAM_APP || '';
+export const TENANT = process.env.LANGSTREAM_TENANT || '';
+export const CONSUMER = process.env.LANGSTREAM_CONSUMER || '';
+export const PRODUCER = process.env.LANGSTREAM_PRODUCER || '';
+export const CREDENTIALS = process.env.LANGSTREAM_CREDENTIALS || '';

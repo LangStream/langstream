5ac2e69d8a73a8224fa6a9478e81a379599046e7
Chris Bartholomew <chris.bartholomew@datastax.com>
2023-10-10 18:04:19 -0400

    Switch the env var prefix to REACT_APP

--- examples/applications/react-chatbot-ui/src/config.ts
+++ examples/applications/react-chatbot-ui/src/config.ts
@@ -14,9 +14,9 @@
 /// limitations under the License.
 ///
 
-export const WEBSOCKET_URL = process.env.LANGSTREAM_WEBSOCKET_URL || '';
-export const APP_NAME = process.env.LANGSTREAM_APP || '';
-export const TENANT = process.env.LANGSTREAM_TENANT || '';
-export const CONSUMER = process.env.LANGSTREAM_CONSUMER || '';
-export const PRODUCER = process.env.LANGSTREAM_PRODUCER || '';
-export const CREDENTIALS = process.env.LANGSTREAM_CREDENTIALS || '';
+export const WEBSOCKET_URL = process.env.REACT_APP_WEBSOCKET_URL || '';
+export const APP_NAME = process.env.REACT_APP_APP || '';
+export const TENANT = process.env.REACT_APP_TENANT || '';
+export const CONSUMER = process.env.REACT_APP_CONSUMER || '';
+export const PRODUCER = process.env.REACT_APP_PRODUCER || '';
+export const CREDENTIALS = process.env.REACT_APP_CREDENTIALS || '';
